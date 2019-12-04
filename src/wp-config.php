eca1c65dbeba5e0a21d8bc2b1dd2a47b274b4aa7
PHUONGNV <phuongnv@soft-flight.work>
2019-12-04 11:40:24 +0700

    [Learning] doc dach

--- src/wp-config.php
+++ src/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'exampleuser');
 define( 'DB_PASSWORD', 'examplepass');
 
 /** MySQL hostname */
-define( 'DB_HOST', 'db');
+define( 'DB_HOST', 'mysql');
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8');
